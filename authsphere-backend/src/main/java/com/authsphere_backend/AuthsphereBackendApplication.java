1366620eba0ff38ecfba07ab62d6097bb57bb41e
kumarmanish562 <qmanishkumar562@gmail.com>
2026-08-29 09:19:33 +0530

    Add initial backend setup with Maven, Spring Boot, and basic configurations

---
+++ authsphere-backend/src/main/java/com/authsphere_backend/AuthsphereBackendApplication.java
@@ -0,0 +1,14 @@
+package com.authsphere_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsphereBackendApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(AuthsphereBackendApplication.class, args);
+	}
+
+}
---
+++ authsphere-backend/src/test/java/com/authsphere_backend/AuthsphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authsphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthsphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
